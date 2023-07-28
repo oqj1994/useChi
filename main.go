61838ebee4b0a3ec6775aa95be20ca84ee397870
jia <carryonb@163.com>
2023-07-28 11:31:56 +0800

    add a picture and create a index page

--- main.go
+++ main.go
@@ -10,7 +10,9 @@ import (
 
 func main() {
 	r := chi.NewRouter()
+	fs := http.FileServer(http.Dir("temps"))
 	r.Use(middleware.Logger)
+	r.Handle("/temps/*", http.StripPrefix("/temps/", fs))
 	r.Get("/time", func(w http.ResponseWriter, r *http.Request) {
 		y, m, d := time.Now().Date()
 		fmt.Fprint(w, fmt.Sprintf("%d/%d/%d", y, m, d))
